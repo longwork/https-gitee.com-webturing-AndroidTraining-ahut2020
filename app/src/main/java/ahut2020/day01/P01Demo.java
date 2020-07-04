9c519b0e3382fa9da2ee49d30ef045177869322a
webturing <zj@webturing.com>
2020-07-04 14:42:37 +0800

    安卓和Java开发环境测试

---
+++ app/src/main/java/ahut2020/day01/P01Demo.java
@@ -0,0 +1,7 @@
+package ahut2020.day01;
+
+public class P01Demo {
+    public static void main(String[] args) {
+        System.out.println("hello ahut！");
+    }
+}
--- app/src/main/java/com/example/androidtraining_ahut2020/MainActivity.java
+++ app/src/main/java/com/example/androidtraining_ahut2020/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.androidtraining_ahut2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button btn=(Button)super.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello Android",Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
