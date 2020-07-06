0fb3d4bd709bc1b420094abc4d967c63aafc15df
webturing <zj@webturing.com>
2020-07-06 14:31:21 +0800

    基本编程练习题册

---
+++ app/src/main/java/ahut2020/p20/P01.java
@@ -0,0 +1,16 @@
+package ahut2020.p20;
+
+import java.util.Random;
+
+//产生随机数的方法: Math.
+// random()
+//Random
+public class P01 {
+    public static void main(String[] args) {
+        System.out.println(Math.random());
+
+        Random r=new Random();
+        System.out.println();
+
+    }
+}
